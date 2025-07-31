ae268c8db0828005090ad62f547f3b3e078aaf48
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-01 00:38:36 +0300

    leaflet css

--- react-leaflet/src/App.jsx
+++ react-leaflet/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <MapContainer center={[9.02497, 38.74689]} zoom={13}>
+      {/* Center of the Map at Addis Ababa */}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
