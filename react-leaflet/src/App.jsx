bf5343fe7a06fa5763d2dad4fd83553f467e8618
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-01 00:34:08 +0300

    Add TileLayer

--- react-leaflet/src/App.jsx
+++ react-leaflet/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import "./App.css";
-import { MapContainer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <MapContainer center={[9.02497, 38.74689]} zoom={13}></MapContainer>;
+  return (
+    <MapContainer center={[9.02497, 38.74689]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
 }
 
 export default App;
