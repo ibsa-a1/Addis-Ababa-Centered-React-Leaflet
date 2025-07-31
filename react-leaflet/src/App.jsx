0860fbe6bb1d799589ab72124e486e19771da82b
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-01 02:50:21 +0300

    some minor changes

--- react-leaflet/src/App.jsx
+++ react-leaflet/src/App.jsx
@@ -40,28 +40,31 @@ function App() {
   });
 
   return (
-    <MapContainer center={[9.02497, 38.74689]} zoom={13}>
-      {/* Center of the Map at Addis Ababa */}
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    <div>
+      <h1>Addis Ababa Centered Map</h1>
+      <MapContainer center={[9.02497, 38.74689]} zoom={13}>
+        {/* Center of the Map at Addis Ababa */}
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-      <MarkerClusterGroup
-        chunkedLoading
-        iconCreateFunction={createClusterCustomIcon}
-      >
-        {markers.map((marker) => (
-          <Marker
-            key={marker.geocode}
-            position={marker.geocode}
-            icon={customIcon}
-          >
-            <Popup>{marker.popup}</Popup>
-          </Marker>
-        ))}
-      </MarkerClusterGroup>
-    </MapContainer>
+        <MarkerClusterGroup
+          chunkedLoading
+          iconCreateFunction={createClusterCustomIcon}
+        >
+          {markers.map((marker) => (
+            <Marker
+              key={marker.geocode}
+              position={marker.geocode}
+              icon={customIcon}
+            >
+              <Popup>{marker.popup}</Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
+      </MapContainer>
+    </div>
   );
 }
 
